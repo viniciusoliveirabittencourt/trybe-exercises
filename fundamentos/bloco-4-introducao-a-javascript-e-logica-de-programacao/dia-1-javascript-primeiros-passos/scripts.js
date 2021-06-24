0a67012f5621936da200679960ea57ace4b39aaa
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-24 12:31:04 -0300

    5 programas de operadores matematicos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,129 @@
-let nome = "Vinicius";
-const  birthCity = "Aracaju";
-let birthYear  = "2003";
-birthYear = "2030";
-birthCity = "Natal";
+//programa de adicao ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let a = 5;
+let b = 8;
+let adicao = a + b;
+console.log(adicao);
 
-console.log(nome);
-console.log(birthCity);
-console.log(birthYear);
+a = 4;
+b = 2;
+adicao = a + b;
+console.log(adicao);
+
+a = 7;
+b = 2;
+adicao = b + a;
+console.log(adicao);
+
+a = 4;
+b = 1;
+adicao = a + b;
+console.log(adicao);
+
+a = 9;
+b = 9;
+adicao = b + a;
+console.log(adicao);
+
+//programa de subtracao ----------------------------------------------------------------------------------
+a = 9;
+b = 8;
+let sub = a - b;
+console.log(sub);
+
+a = 4;
+b = 2;
+sub = a - b;
+console.log(sub);
+
+a = 10;
+b = 8;
+sub = a - b;
+console.log(sub);
+
+a = 4;
+b = 13;
+sub = b - a;
+console.log(sub);
+
+a = 1;
+b = 2;
+sub = a - b;
+console.log(sub);
+
+//programa de multiplicacao *************************************************************************************
+a = 10;
+b = 5;
+let mult = a * b;
+console.log(mult);
+
+a = 2;
+b = 3;
+mult = a * b;
+console.log(mult);
+
+a = 1;
+b = 6;
+mult = a * b;
+console.log(mult);
+
+a = 7;
+b = 5;
+mult = a * b;
+console.log(mult);
+
+a = 4;
+b = 7;
+mult = a * b;
+console.log(mult);
+
+//programa de divisão //////////////////////////////////////////////////////////////////////////////////////////////
+a = 10;
+b = 5;
+let divi = a / b;
+console.log(divi);
+
+a = 4;
+b = 2;
+divi = a / b;
+console.log(divi);
+
+a = 6;
+b = 3;
+divi = a / b;
+console.log(divi);
+
+a = 12;
+b = 6;
+divi = a / b;
+console.log(divi);
+
+a = 10;
+b = 10;
+divi = a / b;
+console.log(divi);
+
+//programa de modulo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+a = 42;
+b = 2;
+let modu = a % b;
+console.log(modu);
+
+a = 69;
+b = 3;
+modu = a % b;
+console.log(modu);
+
+a = 47;
+b = 5;
+modu = a % b;
+console.log(modu);
+
+a = 24;
+b = 4;
+modu = a % b;
+console.log(modu);
+
+a = 8;
+b = 1;
+modu = a % b;
+console.log(modu);
