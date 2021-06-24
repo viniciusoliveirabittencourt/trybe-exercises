72b07409232d0d023549afdff0a3e3f83ec87751
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-24 20:48:02 -0300

    adiciona a atividade 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let som = 0;
+let result;
 for(let impr of numbers){
     console.log(impr);
 }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let som = 0;
+for(let index = 0; index < numbers.length; index += 1){
+    som += numbers[index];
+}
+console.log(som);
