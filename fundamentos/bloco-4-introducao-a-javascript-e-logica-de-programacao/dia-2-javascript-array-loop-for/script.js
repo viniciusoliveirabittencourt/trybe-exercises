964d5f028fec5975d6fb98637462c1cb36a08e52
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-25 12:36:00 -0300

    adiciona a atividade 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
@@ -72,7 +72,7 @@ for (let index = 0; index < numbers.length; index += 1){
 }
 console.log(numMenor);
 
-//atividade 8 e 9 ---------------------------------------------------------------------------------------------------------
+//atividade 8 ---------------------------------------------------------------------------------------------------------
 let vini = [];
 let viniNum = 0;
 
@@ -80,4 +80,18 @@ for (let index = 0; index < 25; index += 1){
     vini.push(viniNum += 1);
 }
 console.log(vini);
-console.log(viniDivAr);
+
+//atividade 9 ---------------------------------------------------------------------------------------------------------
+let vini = [];
+let viniNum = 0;
+
+for (let index = 0; index < 25; index += 1){
+    vini.push(viniNum += 1);
+}
+
+for(let viniDiv of vini){
+    viniDiv = viniDiv / 2;
+    console.log(viniDiv);
+}
+
+console.log(vini);
