13f224ae5c322a25b0359399a0de0c93df1b96b5
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-24 22:49:01 -0300

    adiciona a atividade 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
@@ -45,8 +45,31 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let valorImpar = [];
 
 for (let index = 0; index < numbers.length; index += 1){
-    if(numbers[index] % 2 == 0){
+    if(numbers[index] % 2 == 1){
         valorImpar.push(numbers[index]);
+    }else{
+        console.log("Nenhum valor ímpar encontrado")
     }
 }
 console.log(valorImpar);
+
+//atividade 7 ---------------------------------------------------------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numMaior = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+    if(numMaior < numbers[index]){
+        numMaior = numbers[index];
+    }
+}
+
+let numMenor = numMaior;
+
+for (let index = 0; index < numbers.length; index += 1){
+    if(numMenor > numbers[index]){
+        numMenor = numbers[index];
+    }
+}
+console.log(numMenor);
+
+//atividade 8 ---------------------------------------------------------------------------------------------------------
