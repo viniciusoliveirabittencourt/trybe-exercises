f1e567ea1e827ba09e4405e2fcc0d37433bdafc8
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-24 21:37:52 -0300

    atividade 3 e 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
@@ -1,3 +1,4 @@
+//atividade 1 ---------------------------------------------------------------------------------------------------
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let som = 0;
 let result;
@@ -5,9 +6,26 @@ for(let impr of numbers){
     console.log(impr);
 }
 
+//atividade 2 --------------------------------------------------------------------------------------------------
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let som = 0;
 for(let index = 0; index < numbers.length; index += 1){
     som += numbers[index];
 }
 console.log(som);
+
+//atividade 3 e 4 -----------------------------------------------------------------------------------------------
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let som = 0;
+for(let index = 0; index < numbers.length; index += 1){
+    som += numbers[index];
+}
+let result = som / numbers.length;
+console.log(result);
+
+if(result > 20){
+    console.log("Valor maior que 20");
+}else{
+    console.log("Valor igual ou menor que 20");
+}
+
