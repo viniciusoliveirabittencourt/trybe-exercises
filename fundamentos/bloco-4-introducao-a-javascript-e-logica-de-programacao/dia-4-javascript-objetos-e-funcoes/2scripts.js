bd130475ecf2e31c71b9d4f0db49dcfd096965e0
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-29 11:50:29 -0300

    Primeiro commit do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/2scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/2scripts.js
@@ -1,6 +1,12 @@
 function palindromo(string){
-    let palavraReve;
-    for(let index = 0; index <= string; index += 1){
-        palavraReve = string[index];
+    let stringArr = [];
+    let palavraReve = [];
+    for(let index = 0; index < string.length; index += 1){
+        stringArr.push(string[index]);
     }
+    for(let j = (string.length - 1); j > -1; j -= 1){
+        palavraReve.push(string[j]);
+    }
+    console.log(palavraReve)
 }
+palindromo('paralelepipedo');
