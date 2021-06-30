4d5cb327f0831f48acdf8cc345bed24e61de2e02
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-06-30 19:17:42 -0300

    commit com os exercicios de fixaçao do dia 1 do bloco 5

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/scripts.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Poderoso Chefão';
+
+document.getElementById('second-paragraph').innerText = 'Esse segundo paragrafo tava muito chato';
+document.getElementById('second-paragraph').style.color = 'blue';
+document.getElementById('second-paragraph').style.fontSize = '29px';
