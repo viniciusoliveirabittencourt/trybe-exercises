2c43efc5932736b4e69d828fe9ee9fb37b7e3fd6
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-07-01 16:14:20 -0300

    adicionando a atividade 4, do bloco 4, so queria dizer que estou muito orgulhoso de ter feito esses codigos funcionarem

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/scripts.js
@@ -19,7 +19,7 @@ for(let index = 0; index < valor; index += 1){
 }
 
 //Atividade 3 -----------------------------------------------------------------------------------------------------------------------------------
-let valor = 5;
+let valor = 10;
 let reciveasteric = "";
 let recivespace = "";
 
@@ -32,4 +32,22 @@ for(let secondIndex = 0; secondIndex < valor; secondIndex += 1){
     reciveasteric += "*";
     recivespace = recivespace.slice(0, -1);
     console.log(recivespace + reciveasteric);
+}
+
+//Atividade 4 ----------------------------------------------------------------------------------------------------------------------------------
+let valor = 25;
+let reciveasteric = '*';
+let recivespace = '';
+
+if (valor % 2 === 0){
+    console.log('Pf coloque apenas impares');
+} else {
+    for(let index = 0; index < (valor / 2); index += 1){
+        recivespace += ' '
+    }
+    for(let secondIndex = 0; secondIndex < valor; secondIndex += 2){
+        recivespace = recivespace.slice(0, -1);
+        console.log(recivespace + reciveasteric);
+        reciveasteric += "**";
+    }
 }
