090b451a8a786a71213119c2a11bf4c328326377
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-07-02 15:19:48 -0300

    corrigindo o palindromo do exercicio II do bloco 4 dia 4 obs: vai ficar com esse undefined ali em baixo mesmo pq eu n consegui corrigir

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/2scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/2scripts.js
@@ -1,26 +1,34 @@
 function palindromo(string){
-    let stringArr = [];
     let palavraReve = [];
+    let valor = 0;
     let verificaPalindrome;
 
-    for(let index = 0; index < string.length; index += 1){
-        stringArr.push(string[index]);
-    }
     for(let j = (string.length - 1); j > -1; j -= 1){
         palavraReve.push(string[j]);
     }
-
-    for (var i = 0, len = stringArr.length; i < len; i++){
-        if(string[i] == ' '){
-            console.log('Pf, apenas use palavras');
-        }else if(stringArr[i] == palavraReve[i]){
-            verificaPalindrome = true;
-        }else{
-            verificaPalindrome = false;
+    
+    for (let index in string){
+        if (string[index] === palavraReve[index]){
+            valor += 1;
         }
     }
+
+    if (valor == string.length){
+        verificaPalindrome = true;
+    } else {
+        verificaPalindrome = false;
+    }
+
+    for (let second in string){
+        if (string[second] == ' '){
+            verificaPalindrome = console.log('Pf use apenas palavras');
+        }
+    }
+    
     return verificaPalindrome;
 }
+console.log(palindromo('puta merda'));
+
 function verificaMaior(arr){
     let maiorNum;
     for(let index = 0; index < arr; index += 1){
