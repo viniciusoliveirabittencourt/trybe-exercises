62ecd765e00f913e7b977d6c06fca990de3cba76
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-07-08 13:51:37 -0300

    exercicio de fixaçao

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
@@ -5,45 +5,51 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 const container = document.querySelector('.container');
 
-container.addEventListener('click', addTech);
-firstDiv.addEventListener('click', addTech);
-secondDiv.addEventListener('click', addTech);
-thirdDiv.addEventListener('click', addTech);
-
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+1.Copie esse arquivo e edite apenas ele;
+2.Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+3.Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+4.Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+4.1. Que tal redirecionar para seu portifólio?
+5.Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstDiv.addEventListener('dblclick', resetText);
-// Não precisa passar o parâmetro dentro do addEventListener. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'divUm'.
-
-function addTech(){
-    let containerFilhos = container.children;
-    for (let index = 0; index < containerFilhos.length; index += 1){
-        if (containerFilhos[index].className = 'tech'){
-            containerFilhos[index].classList.remove('tech');
-        } else {
-                console.log(secondDiv.classList.add('tech'));
+function addTech(event) {
+    let containerChildre = container.children;
+    for (let index = 0; index < containerChildre.length; index += 1) {
+        if (containerChildre[index].className.includes('tech')) {
+            containerChildre[index].classList.remove('tech');
         }
     }
+
+    event.target.className = 'tech';
 }
+
+function mudaText() {
+    document.querySelector('.tech').innerText = input.value;
+}
+
+function redPag() {
+    window.location.href = 'https://viniciusoliveirabittencourt.github.io/';
+}
+
+function changeColor() {
+    myWebpage.style.color = '#2fc18c';
+}
+function resetColor() {
+    myWebpage.style.color = 'white';
+}
+
+firstDiv.addEventListener('click', addTech);
+secondDiv.addEventListener('click', addTech);
+thirdDiv.addEventListener('click', addTech);
+input.addEventListener('keyup', mudaText);
+myWebpage.addEventListener('dblclick', redPag);
+myWebpage.addEventListener('mouseover', changeColor);
+myWebpage.addEventListener('mouseleave', resetColor);
